f19e7541b2a2e405e0546d52d80506f32f1fc780
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2022-12-01 19:19:45 +0200

    fix(day01): part two

--- day01/day01.go
+++ day01/day01.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -13,9 +14,8 @@ func check(e error) {
 	}
 }
 
-func main() {
-
-	file, err := os.Open("input.txt")
+func GetHighetstElfCalories(inputFilePath string) {
+	file, err := os.Open(inputFilePath)
 	check(err)
 
 	fileScanner := bufio.NewScanner(file)
@@ -23,27 +23,42 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	currentElfCalories := 0
-	highestElfCalories := 0
+	allElfs := []int{}
+
+	var highestElfCalories [3]int
 
 	for fileScanner.Scan() {
 		currentLine := fileScanner.Text()
 
 		if currentLine == "" {
-			fmt.Println(currentElfCalories)
-			fmt.Println("------")
+			// fmt.Println(currentElfCalories)
+			allElfs = append(allElfs, currentElfCalories)
 			currentElfCalories = 0
 		} else {
 			currentLine, err := strconv.Atoi(currentLine)
 			check(err) // possible input text/string
 
 			currentElfCalories += int(currentLine)
-			// fmt.Println(currentElfCalories)
 
-			if currentElfCalories > highestElfCalories {
-				highestElfCalories = currentElfCalories
+			if currentElfCalories > highestElfCalories[0] {
+				highestElfCalories[0] = currentElfCalories
 			}
 		}
 	}
 
-	fmt.Println("Highest Elf with Calories ", highestElfCalories)
+	// fmt.Println("Highest Elf with Calories ", highestElfCalories)
+
+	// Sort the elfs and get the top 3 elfs by calories
+	sort.Ints(allElfs)
+	topElfCalories := 0
+	for i := 1; i <= 3; i++ {
+		elfLength := len(allElfs)
+		topElfCalories += allElfs[elfLength-i]
+	}
+	fmt.Println(topElfCalories)
+
+}
+
+func main() {
+	GetHighetstElfCalories("input.txt")
 }
